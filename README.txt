8375240458e730cda7a4f1fdb97758d4c9dd7b2f
Kelson <kelson.felp@gmail.com>
2023-11-21 00:01:36 -0300

    1.0

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 Integrantes
 
 Kelson Felipe Budin de Almeida - 2312697
+Willian Giacomelli Silva - 2312182
--- dijkstra.c
+++ dijkstra.c
@@ -2,6 +2,7 @@
 
 //Integrantes 
 //Kelson Felipe Budin de Almeida - 2312697
+//Willian Giacomelli Silva - 2312182
 
 
 #define V 6
